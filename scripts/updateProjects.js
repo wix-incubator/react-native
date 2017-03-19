19718636ab395612adabc634d3a6e77282792d7a
Elad Bogomolny <eladbo@wix.com>
2017-03-19 17:01:08 +0200

    added fs, changed projects to json file, added first complete prototype for auto updating

--- scripts/updateProjects.js
+++ scripts/updateProjects.js
@@ -1,18 +1,29 @@
 var request = require("request")
+var fs = require('fs');
 var projectsData = require('../src/projects.json')
 
+function updateProjects(projectsJson) {
+    console.log(projecsJson)
+    fs.writeFileSync('../src/projects.json', projectsJson);
+}
+        
 projectsData.onlineProjectList.forEach(function(projectUrl){
     request({
     url: projectUrl,
     json: true
     }, function (error, response, body) {
 
-    if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-        if ((((((body.title) && (body.description)) && body.title) && body.github) && body.size) && ((body.AndroidVideoUrl || body.IOSVideoUrl) || body.image)) {
-            console.log('approved')
-            
+        if (!error && response.statusCode === 200) {
+            console.log(body) // Print the json response
+            if (((((((body.title) && (body.description)) && body.title) && body.github) && body.size) && body.name) && ((body.AndroidVideoUrl || body.IOSVideoUrl) || body.image)) {
+                console.log('approved')
+                projectsData.projectList.push(body.name)
+                projectsData[body.name] = body
+            } else {
+                console.log('project not approved')
+            }
         }
-    }
+    })
 })
-})
+
+updateProjects(projectsData)
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from './components/Header'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 
-import projects from './projects.js'
+import projects from './projects.json'
 
 class App extends Component {
   render() {
