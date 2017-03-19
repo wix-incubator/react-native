f4d5d79483dff1c4f01300e7ba12a9ce0a25c137
Elad Bogomolny <eladbo@wix.com>
2017-03-19 14:31:07 +0200

    updateProjects script and readme

--- scripts/updateProjects.js
+++ scripts/updateProjects.js
@@ -1,2 +1,18 @@
 var request = require("request")
 var projectsData = require('../src/projects.json')
+
+projectsData.onlineProjectList.forEach(function(projectUrl){
+    request({
+    url: projectUrl,
+    json: true
+    }, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        console.log(body) // Print the json response
+        if ((((((body.title) && (body.description)) && body.title) && body.github) && body.size) && ((body.AndroidVideoUrl || body.IOSVideoUrl) || body.image)) {
+            console.log('approved')
+            
+        }
+    }
+})
+})
